a031329def63c2710448c6420fe4ba336b77d533
harihitode <harihitode@gmail.com>
2016-12-06 19:25:55 +0900

    fix: running on windows

--- conf/face.el
+++ conf/face.el
@@ -24,30 +24,30 @@
   (set-face-attribute 'whitespace-empty nil
                       :background bg-color))
 
-;; emacs alpha
-(if window-system (progn (set-frame-parameter nil 'alpha 85)))
-
 ;; Appearance
-(menu-bar-mode 0) ; menubar (upper) disable
+(if window-system (progn
+                    (set-frame-parameter nil 'alpha 95)
+                    (tool-bar-mode 0) ; toolbar (upper) disable
+                    (scroll-bar-mode 0) ; scrollbar disable
+                    (global-set-key (kbd "C-z") #'eshell)
+                    (load-theme 'tango-dark t)
+                    (set-face-attribute 'mode-line nil
+                                        :box nil ; disable 3d effect
+                                        :foreground "black"
+                                        :background "#65ace4"
+                                        :overline nil
+                                        :underline nil)
+
+                    (set-face-attribute 'mode-line-inactive nil
+                                        :box nil
+                                        :foreground "grey30"
+                                        :background "pink"
+                                        :overline nil
+                                        :underline nil)))
+(menu-bar-mode 0)
 (when (eq window-system 'ns)
   ;; for Cocoa
-  (global-set-key (kbd "C-z") #'eshell)
-  (tool-bar-mode 0) ; toolbar (upper) disable
-  (scroll-bar-mode 0) ; scrollbar disable
-  (load-theme 'tango-dark t)
-  (set-face-attribute 'mode-line nil
-                      :box nil ; disable 3d effect
-                      :foreground "black"
-                      :background "yellow"
-                      :overline nil
-                      :underline nil)
-
-  (set-face-attribute 'mode-line-inactive nil
-                      :box nil
-                      :foreground "grey30"
-                      :background "grey85"
-                      :overline nil
-                      :underline nil))
+)
 
 ;; Mode Line Setting
 (setq-default mode-line-format (list "$ %b (L%l, C%c) <%m>"))
