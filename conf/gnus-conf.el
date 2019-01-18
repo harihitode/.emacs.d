c0f1d108c54daae2309ad380fa6a833cd7dac9b4
Daichi Murakami <harihitode@gmail.com>
2019-01-18 09:47:44 +0900

    gnus update

--- conf/gnus-conf.el
+++ conf/gnus-conf.el
@@ -3,6 +3,7 @@
 (require 'nnimap)
 (require 'smtpmail)
 (require 'bbdb)
+(require 'starttls)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;MEMO;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -12,6 +13,14 @@
 ;; User defined key (Actually, I want to use BBDB ...)
 ;; `user-mail-address'
 ;; `user-full-name'
+
+(setq smtp-accounts
+      '((starttls "your_mail" "mail_server 1" 587)
+        (starttls "another_your_mail" "mail_server 2" 587)
+        (starttls "yet_another_your_mail" "mail_server 3" 587)))
+
+(setq user-mail-address "your_mail")
+(setq user-full-name "your_name")
 
 (setq read-mail-command 'gnus
       mail-user-agent 'gnus-user-agent)
@@ -35,11 +44,16 @@
 
 (defun set-smtp (authmech server port)
   "Set the proper protocol, server and port for sending mail"
-  (setq smtpmail-smtp-server server
-        smtpmail-smtp-service port)
+  (setq message-send-mail-function 'smtpmail-send-it
+        smtpmail-starttls-credentials '(("smtp.gmail.com" 25 nil nil))
+        smtpmail-auth-credentials '(("smtp.gmail.com" 25 "your_gmail@gmail.com" nil))
+        smtpmail-default-smtp-server server
+        smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 25
+        smtpmail-local-domain "localhost")
   (cond ((eql authmech 'starttls)
-         (setq smtpmail-stream-type 'startssl)
-         (message (format "startssl %s:%s" server port)))
+         (setq smtpmail-stream-type 'starttls)
+         (message (format "starttls %s:%s" server port)))
         ((eql authmech 'ssl)
          (setq smtpmail-stream-type 'ssl)
          (message (format "ssl %s:%s" server port)))
@@ -59,13 +73,14 @@
              do (cl-return (progn
                              (apply 'set-smtp (list authmech server port))
                              (apply f args)))
-             finally (error  (format "Cannot infe SMTP information. %s" from)))))
+             finally (error  (format "There is no SMTP information. %s" from)))))
 
 (advice-add #'message-send :around #'message-send-around)
 
 ;; address settings
 (setq bbdb/news-auto-create-p t)
-(bbdb-initialize 'gnus 'message)
+(bbdb-mail-aliases)
+(bbdb-initialize 'gnus 'message 'aliases)
 (add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
 (add-hook 'message-setup-hook 'bbdb-insinuate-message)
 (setq bbdb/news-auto-create-p 'bbdb-ignore-most-messages-hook)
